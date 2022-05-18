dabaca9c3a813704cf48c887fb7ba351b289ef35
Aladale Snorlax <aladales@si-e2.kzn.21-school.ru>
2022-05-18 23:07:17 +0300

    upd

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,118 +1,80 @@
 #include "s21_cat.h"
 
-int main(int argc, char* argv[]) {
-   int tmp = 0, check = 1;
-   char **p_str = NULL;
-   char *str = NULL;
-   struct flags ptr;
-   init_struct_flags(&ptr);
-   if (argc > 0)
-    {
-        for (int i = 1; i < argc; i++)
-        {
-            for (int y = 0; argv[i][y]; y++)
-            {
-                    if (argv[i][y] != '-')
-                    {
-                        ptr.name_file += 1;
-                        p_str = realloc(p_str, ptr.name_file * sizeof(char*));
-                        
-                        for (int k = 0; k < ptr.name_file; k++)
-                        {
-                            p_str[i] = realloc(str, tmp * sizeof(char));
+int main(int argc, char *argv[]) {
+    int *position_str = NULL;
+    struct flags ptr;
+    init_struct_flags(&ptr);
+    if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            for (int y = 0; argv[i][y]; y++) {
+                if (argv[i][y] != '-') {
+                    ptr.name_file += 1;
+                    position_str =
+                        realloc(position_str, (ptr.name_file) * sizeof(int));
+                    position_str[ptr.name_file - 1] = i;
+                    break;
+                } else if (check_flags_linux(argv[i], &ptr) && ptr.name_file == 0) {
+                    break;
+                } else if(ptr.name_file == 0) {
+                    for (int j = y + 1; argv[i][j]; j++) {
+                        if (!check_flags_bash(argv, i, j, &ptr)) {
+                            printf("cat: неверный ключ — «%c»\n", argv[i][j]);
                         }
-                        
-                        // for (int j = y; argv[i][j]; j++)
-                        // {
-                        //   if (str = realloc(str, (tmp + 1) * sizeof(char)))
-                        //     {
-                        //         str[tmp] = argv[i][j];
-                        //         tmp++;
-                        //     } else {
-                        //         if (str != NULL) {
-                        //             free(str);
-                        //         }
-                        //         exit(2);
-                        //         printf("memory error");
-                        //     } }                                  
-                             break;
-                    } else if (check_flags_linux(argv[i], &ptr)) {
-                     break;
-                    } else
-                    {
-                        for (int j = y + 1; argv[i][j]; j++)
-                        {
-                            if (!check_flags_bash(argv, i, j, &ptr)) {
-                                printf("cat: неверный ключ — «%c»\n", argv[i][j]);
-                                exit(1);
-                            }
-                        }
-                        break;
-                    } 
-                }    
+                    }
+                    break;
+                }
+            }
+        }
+        if (position_str) {
+            check_file(argv, &ptr, position_str);
         }
     }
     return 0;
 }
 int check_flags_linux(char *argv, struct flags *ptr) {
     int res = 0;
-    if (!strcmp(argv, "--number-nonblank"))
-    {
-       ptr->b = 1;
-       res = 1;
-    } else if (!strcmp(argv, "--number"))
-    {
+    if (!strcmp(argv, "--number-nonblank")) {
+        ptr->b = 1;
+        res = 1;
+    } else if (!strcmp(argv, "--number")) {
         ptr->n = 1;
-       res = 2;
-    } else if (!strcmp(argv, "--squeeze-blank"))
-    {
+        res = 2;
+    } else if (!strcmp(argv, "--squeeze-blank")) {
         ptr->s = 1;
-       res = 3;
+        res = 3;
     } else {
         printf("cat: нераспознанный параметр «%s»\n", argv);
-        exit(3);
     }
-    return res;    
+    return res;
 }
 
 int check_flags_bash(char *argv[], int i, int y, struct flags *ptr) {
     int res = 0;
-    if (argv[i][y] == 'b')
-    {
-       res = 1;
-       ptr->b = 1;
-    } else if (argv[i][y] == 'e')
-    {
-       ptr->e = 1;
-       ptr->v = 1;
-       res = 2;
-    } else if (argv[i][y] == 'n')
-    {
-       ptr->n = 1;
-       res = 3;
+    if (argv[i][y] == 'b') {
+        res = 1;
+        ptr->b = 1;
+    } else if (argv[i][y] == 'e') {
+        ptr->e = 1;
+        ptr->v = 1;
+        res = 2;
+    } else if (argv[i][y] == 'n') {
+        ptr->n = 1;
+        res = 3;
+    } else if (argv[i][y] == 's') {
+        ptr->s = 1;
+        res = 4;
+    } else if (argv[i][y] == 't') {
+        ptr->t = 1;
+        ptr->v = 1;
+        res = 5;
+    } else if (argv[i][y] == 'T') {
+        ptr->t = 1;
+        res = 6;
+    } else if (argv[i][y] == 'E') {
+        ptr->e = 1;
+        res = 7;
     }
-    else if (argv[i][y] == 's')
-    {
-       ptr->s = 1;
-       res = 4;
-    }
-    else if (argv[i][y] == 't')
-    {
-       ptr->t = 1;
-       ptr->v = 1;
-       res = 5;
-    }
-    else if (argv[i][y] == 'T')
-    {
-       ptr->t = 1;
-       res = 6;
-    }
-    else if (argv[i][y] == 'E')
-    {
-       ptr->e = 1;
-       res = 7;
-    }
-    return res;    
+    return res;
 }
 void init_struct_flags(struct flags *ptr) {
     ptr->b = 0;
@@ -122,13 +84,52 @@ void init_struct_flags(struct flags *ptr) {
     ptr->t = 0;
     ptr->v = 0;
 }
-char*** realisation_str(int n) {
-    char *** temp = (char ***) malloc(n * sizeof(char **));
-    for (int i=0; i < n; i++) {
-        temp[i] = (char **) malloc(n * sizeof(char*));
-        for (int j = 0; j < n; j++)
-            temp[i][j] = (char *) malloc(n * sizeof(char*));   
+
+void check_file(char *argv[], struct flags *ptr, int *position_str) {
+    FILE *ptr_file;
+    char c;
+    char *str = NULL;
+    int numiration = 1;
+    int j;
+    for (int i = 0; i < ptr->name_file; i++) {
+        if ((ptr_file = fopen(argv[position_str[i]], "r")) == NULL) {
+            printf("ERROR\n");
+        } else {
+            for (j = 0; (c = fgetc(ptr_file)) != EOF; j++) {
+                str = realloc(str, (j + 1) * sizeof(char));
+                str[j] = c;
+            }
+            str[j] = '\0';
+
+            output_file_with_flags(ptr, str, &numiration);
+        }
     }
 
-    return temp;
+    
+}
+
+void output_file_with_flags(struct flags *ptr, char *str, int *numiration) {
+    int check = 1;
+    if (ptr->s) {
+        for (int i = 0; str[i]; i++) {
+            if (str[i] != ' ') {
+                check = 0;
+            }
+        }
+        if (check) {
+            return;
+        }
+    }
+    if (ptr->b) {
+        for (int i = 0; str[i]; i++) {
+            if (str[i] != ' ') {
+                printf("     %d", *numiration);
+                break;
+            }
+        }
+    }
+    if (ptr->n && ptr->b == 0) {
+        printf("     %d", *numiration);
+    }
+    printf("%s", str);
 }
--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -8,14 +8,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct  flags
-{
+struct flags {
     int b, e, n, s, t, v, name_file;
 };
 
 int check_flags_linux(char *argv, struct flags *ptr);
-int check_flags_bash(char *argv[], int i, int y,  struct flags *ptr);
+int check_flags_bash(char *argv[], int i, int y, struct flags *ptr);
+void output_file_with_flags(struct flags *ptr, char *str, int *numiration);
+void check_file(char *argv[], struct flags *ptr, int *position_str);
 void init_struct_flags(struct flags *ptr);
-char*** realisation_str(int n);
 
-#endif // SRC_S21_CAT_H_
+#endif  // SRC_S21_CAT_H_
